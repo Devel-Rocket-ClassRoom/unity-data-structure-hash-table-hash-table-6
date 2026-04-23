f38ea2a4da755a830af6516223efedd44dca25b8
김유찬 <152022287+muchan918@users.noreply.github.com>
2026-04-23 16:36:21 +0900

    gitignore수정/SimpleHashTable 스켈레톤 구현

--- Assets/Scripts/SimpleHashTable.cs
+++ Assets/Scripts/SimpleHashTable.cs
@@ -1,16 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class SimpleHashTable : MonoBehaviour
+public class SimpleHashTable<TKey, TValue> : IDictionary<TKey, TValue>
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public TValue this[TKey key] { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+    public ICollection<TKey> Keys => throw new System.NotImplementedException();
+
+    public ICollection<TValue> Values => throw new System.NotImplementedException();
+
+    public int Count => throw new System.NotImplementedException();
+
+    public bool IsReadOnly => throw new System.NotImplementedException();
+
+    public void Add(TKey key, TValue value)
     {
-        
+        throw new System.NotImplementedException();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Add(KeyValuePair<TKey, TValue> item)
     {
-        
+        throw new System.NotImplementedException();
+    }
+
+    public void Clear()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool Contains(KeyValuePair<TKey, TValue> item)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool ContainsKey(TKey key)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool Remove(TKey key)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool Remove(KeyValuePair<TKey, TValue> item)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool TryGetValue(TKey key, out TValue value)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
     }
 }
